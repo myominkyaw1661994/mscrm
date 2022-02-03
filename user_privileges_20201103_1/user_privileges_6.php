268c6058cea7ac31c3cb2c588d28e0a56aa2cc52
myo min kyaw <m2k1661994@gmail.com>
2022-02-03 16:33:24 +0630

    remove user privileges

--- user_privileges_20201103_1/user_privileges_6.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//This is the access privilege file
-$is_admin=true;
-
-$user_info=array('user_name'=>'Yoshio Yamamoto','is_admin'=>'on','user_password'=>'$2y$10$GaSj.ZJN/CQyg/O3NBXoJOq83TPKIoJJer79jSFvnd.RRD5G3fnT6','confirm_password'=>'$2y$10$kAT.Zy.8hfRymsx3jye0DOz0pzlsSrlakDUF1nBtSy7fo2UiTEkbS','first_name'=>'Yoshio','last_name'=>'Yamamoto','roleid'=>'H2','email1'=>'y.yamac1948@gmail.com','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'','start_hour'=>'09:00','is_owner'=>'','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'yyyy-mm-dd','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'wEswrFoCUacHvArE','time_zone'=>'UTC','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'en_us','reminder_interval'=>'','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Sunday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'medium','defaulteventstatus'=>'','defaultactivitytype'=>'','hidecompletedevents'=>'0','defaultcalendarview'=>'','currency_name'=>'USA, Dollars','currency_code'=>'USD','currency_symbol'=>'&#36;','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','id'=>'6');
-
-?>
--- user_privileges_20201103_3/user_privileges_10.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//This is the access privilege file
-$is_admin=true;
-
-$user_info=array('user_name'=>'Zar Zar Lwin','is_admin'=>'on','user_password'=>'$2y$10$60kVg7buoQjA/mRylnjSROPkYqOOlvDWgPqEB3AFUGd3AnOskJTwK','confirm_password'=>'$2y$10$D4IN6ege10X6X29Cy/Ka6erCCckAfgYcl2Bgf5Kmlc4TYCsUE57Pq','first_name'=>'Zar Zar','last_name'=>'Lwin','roleid'=>'H6','email1'=>'zarzarlwin.89@gmail.com','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'','start_hour'=>'09:00','is_owner'=>'','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'yyyy-mm-dd','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'4weHI9pYPS3U00Yt','time_zone'=>'UTC','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'en_us','reminder_interval'=>'','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Sunday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'medium','defaulteventstatus'=>'','defaultactivitytype'=>'','hidecompletedevents'=>'0','defaultcalendarview'=>'','currency_name'=>'USA, Dollars','currency_code'=>'USD','currency_symbol'=>'&#36;','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','id'=>'10');
-
-?>
